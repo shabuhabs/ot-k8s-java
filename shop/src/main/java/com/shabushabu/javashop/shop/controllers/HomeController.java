f79fd417d1cc8f26d411b53750c1c90d0ada1228
shabuhabs <ddesjardins@splunk.com>
2021-01-13 14:26:10 -0700

    OT impl

--- shop/src/main/java/com/shabushabu/javashop/shop/controllers/HomeController.java
+++ shop/src/main/java/com/shabushabu/javashop/shop/controllers/HomeController.java
@@ -35,70 +35,52 @@ public class HomeController {
 
    @RequestMapping(value="/", method = RequestMethod.GET)
    public String usingRequestParam(Model model, @RequestParam(value="name", required=true) String thename, @RequestParam(value="color", required=true) String thecolor) {
-     
-/*
-     final Span s_tracer.buildSpan("usingRequestParam").start();
-     try (Scope scope = s_tracer.scopeManager().activate(span)) {
-         span.setTag("name",thename);
-         span.setTag("favcolor", thecolor);
-	 	// ORIGINAL CODE
-         	model.addAttribute("user", new User());
-        	 model.addAttribute("products", productService.getProducts());
-	 	// END ORIGINAL CODE	   	     
-         } finally {
-           span.finish();
-	 }
-*/
+
 	 	// Create Span
 	   Span span = s_tracer.spanBuilder("usingRequestParam").startSpan();
 	   	// Put the span into the current Context
 	   try (Scope scope = span.makeCurrent()) {
-	        // Set Name tag: This will be our unique way to search for a trace, by specific user at a specifc time in UI.
+	     
+		 // Set Name tag: This will be our unique way to search for a trace, by specific user at a specifc time in UI.
 			span.setAttribute("name",thename);
 		// Set Favorite Color tag: This will allow us to see traffic by "favcolor" in UI.
 			span.setAttribute("favcolor", thecolor);
-                // ORIGINAL CODE
+        // ORIGINAL CODE
 			model.addAttribute("user", new User());
 			model.addAttribute("products", productService.getProducts());
 		// END ORIGINAL CODE
-	        //
-		//
+	   
 	        
 		} finally {
-	          span.end(); // closing the scope does not end the span, this has to be done manually
+	          span.end(); 
 	   	}
 
-
-// Start a span with scope
-//        Span manualSpan = tracer.spanBuilder("manualSpan") // operation name
-//        	       .setSpanKind(Span.Kind.SERVER) // tag the span as a service boundary
-//        	       	       .startSpan();
-//        	       	              try (Scope scope = manualSpan.makeCurrent()) {
-//        	       	                       // Add attributes
-//        	       	                                manualSpan.setAttribute("my.key", "myvalue");
-//        	       	                                	 wait(10);
-//        	       	                                	          } finally {
-//        	       	                                	                   // End span
-//        	       	                                	                            manualSpan.end();
-//        	       	                                	                            	 } // finally
-//        	       	                                	                            	       // End manual span stanza     
      return "index";
    } 
-    @PostMapping("/adduser")
+    
+   
+   
+   @PostMapping("/adduser")
     public String addUser(@ModelAttribute User user, Model model) {
-    	 final Tracer s_tracer = GlobalTracer.get();
-         final Span span = s_tracer.buildSpan("adduser").start();
-         try (Scope scope = s_tracer.scopeManager().activate(span)) {
+    	 
+	// Create Span
+	   Span span = s_tracer.spanBuilder("usingRequestParam").startSpan();
+	   	// Put the span into the current Context
+	   try (Scope scope = span.makeCurrent()) {
 	     
-             span.setTag("name", user.getName());
-             span.setTag("favcolor", user.getColor());
-             // ORIGINAL CODE
-	     model.addAttribute("products", productService.getProducts()); 	
-	     // END ORIGINAL CODE
-         } finally {
-               span.finish();
-         }		       
-         return "index";
+		 // Set Name tag: This will be our unique way to search for a trace, by specific user at a specifc time in UI.
+			span.setAttribute("name",thename);
+		// Set Favorite Color tag: This will allow us to see traffic by "favcolor" in UI.
+			span.setAttribute("favcolor", thecolor);
+        // ORIGINAL CODE
+			model.addAttribute("products", productService.getProducts());
+		// END ORIGINAL CODE
+	   
+	        
+		} finally {
+	          span.end(); 
+	   	}
+
     }
  
 }
